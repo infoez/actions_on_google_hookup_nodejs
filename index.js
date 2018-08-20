5285cb98d2da889f160258bf9a814bc90e9787c8
voodoochild <voodoochilds@live.co.kr>
2018-08-20 21:31:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const index = express();
+
+index.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+index.use(bodyParser.json());
+
+index.post("/webhook", function (req, res) {
+    var speech =
+        req.body.queryResult &&
+        req.body.queryResult.parameters &&
+        req.body.queryResult.parameters.echoText ? req.body.queryResult.parameters.echoText : "Seems like some problem. Speak again.";
+    return res.json({
+        fulfillmentText: speech,
+        source: "webhook test"
+    });
+});
+
+index.listen(process.env.PORT || 8000, function () {
+    console.log("Server up and listening");
+});
